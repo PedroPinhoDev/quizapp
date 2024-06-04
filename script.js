74623d5069c4f21ccdf1bf6e0032213836b11864
Pedro Pinho <pedrohpinho1977@gmail.com>
2024-06-04 03:10:04 +0000

    Alteração dinâmica do assuto da página

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ botoesAssunto.forEach(botao => {
     botao.addEventListener("click", selecionarAssunto)
 })
 
-function selecionarAssunto() {
+function selecionarAssunto(evento) {
     const assunto = evento.target.innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
